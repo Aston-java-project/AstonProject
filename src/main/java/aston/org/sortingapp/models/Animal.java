ad2a7ddb4f00ad213c70027768e58a0e4bd89ed3
besenevaj <besenevaj@gmail.com>
2024-08-12 17:13:41 +0400

    Создан класс Animal

--- src/main/java/aston/org/sortingapp/models/Animal.java
+++ src/main/java/aston/org/sortingapp/models/Animal.java
@@ -1,7 +1,67 @@
 package aston.org.sortingapp.models;
 
-public class Animal {
+public class Animal implements Comparable<Animal> {
     /*Имплементирует интерфейс Comparable или Comparator.
     Также должен реализовывать паттерн Builder
      */
+    private final String species;
+    private final String eyeColor;
+    private final String fur;
+
+    private Animal(Builder builder) {
+        this.species = builder.species;
+        this.eyeColor = builder.eyeColor;
+        this.fur = builder.fur;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public String getFur() {
+        return fur;
+    }
+
+    @Override
+    public int compareTo(Animal other) {
+        return this.species.compareTo(other.species);
+    }
+
+    public static class Builder {
+        private String species;
+        private String eyeColor;
+        private String fur;
+
+        public Builder setSpecies(String species) {
+            this.species = species;
+            return this;
+        }
+
+        public Builder setEyeColor(String eyeColor) {
+            this.eyeColor = eyeColor;
+            return this;
+        }
+
+        public Builder setFur(String fur) {
+            this.fur = fur;
+            return this;
+        }
+
+        public Animal build() {
+            return new Animal(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Животное{" +
+                "вид='" + species + '\'' +
+                ", цвет глаз='" + eyeColor + '\'' +
+                ", шерсть='" + fur + '\'' +
+                '}';
+    }
 }
