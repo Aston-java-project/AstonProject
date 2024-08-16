ed3b13d142096cfa99fc0d5a589c976abd6601d0
askv1 <99121657+askv1@users.noreply.github.com>
2024-08-16 19:09:05 +0300

    improve exception handling for file deserialization

--- src/main/java/aston/org/sortingapp/input/InputFromFile.java
+++ src/main/java/aston/org/sortingapp/input/InputFromFile.java
@@ -1,9 +1,8 @@
 package aston.org.sortingapp.input;
 
 import aston.org.sortingapp.models.*;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.ObjectInputStream;
+
+import java.io.*;
 
 public class InputFromFile<T> extends AbstractInputMethod<T> {
 
@@ -29,7 +28,14 @@ public class InputFromFile<T> extends AbstractInputMethod<T> {
             return true;
         } catch (FileNotFoundException e) {
             System.out.println("Отсутствует файл с данными!");;
+        } catch (EOFException e) {
+            System.out.println("Неожиданный конец файла!");
+        } catch (StreamCorruptedException e) {
+            System.out.println("Данные повреждены или не соотвутствуют формату");
+        } catch (IOException e) {
+            System.out.println("Ошибка ввода/вывода");
         } catch (Exception e) {
+            System.err.println("Произошла непредвиденная ошибка: " + e.getMessage());
             e.printStackTrace();
         }
         return false;
